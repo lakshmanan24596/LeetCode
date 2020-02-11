e7eb3b435c327a9888fd1280faa774c2ec742ba3
lakshmanan24596 <31705646+lakshmanan24596@users.noreply.github.com>
2020-02-11 18:40:33 +0530

    Update 6. ZigZag Conversion.java

--- algo/6. ZigZag Conversion.java
+++ algo/6. ZigZag Conversion.java
@@ -1,3 +1,26 @@
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+
+// Write the code that will take a string and make this conversion given a number of rows:
+// string convert(string s, int numRows);
+
+// Example 1:
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+
+// Example 2:
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+
 class Solution 
 {
     public String convert(String s, int numRows) 
@@ -10,15 +33,16 @@ class Solution
         
         int noOfRows = Math.min(length, numRows);
         for(int i=0; i<noOfRows; i++)
-        {
             list.add(new StringBuilder());
-        }
+        
         
         int row = 0;
         boolean flipDir = false;
+        StringBuilder sb;
+        
         for(int i=0; i<length; i++)
         {
-            StringBuilder sb = list.get(row);
+            sb = list.get(row);
             sb.append(s.charAt(i));
             if(row == 0 || row == noOfRows-1)
                 flipDir = !flipDir;
@@ -27,9 +51,8 @@ class Solution
         
         StringBuilder output = new StringBuilder();
         for(int i=0; i<noOfRows; i++)
-        {
             output = output.append(list.get(i));
-        }
+
         return output.toString();
     }
 }
