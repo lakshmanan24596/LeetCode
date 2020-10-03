ce66073fd0980d98b1197b75e519d76f2502181e
lakshmanan24596 <lakshmanan24596@gmail.com>
2020-10-03 14:00:09 +0530

    add

---
+++ algo/0394. Decode String.java
@@ -0,0 +1,75 @@
+/*
+Given an encoded string, return its decoded string.
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. 
+Note that k is guaranteed to be a positive integer.
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+Example 1:
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+
+Example 2:
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+
+Example 3:
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+
+Example 4:
+Input: s = "abc3[cd]xyz"
+Output: "abccdcdcdxyz"
+*/
+
+class Solution
+{
+    public String decodeString(String input)
+    {
+        StringBuilder output = new StringBuilder();
+        int len = input.length();
+        
+        for(int i = 0; i < len; i++)
+        {
+            char ch = input.charAt(i);
+            if(Character.isDigit(ch))
+            {
+                int count = Character.getNumericValue(ch);
+                while(Character.isDigit(input.charAt(i+1))) 
+                {
+                    count = (count * 10) + Character.getNumericValue(input.charAt(++i)); // ex: 32 = (3 * 10) + 2;
+                }
+                
+                int braceCount = 0, endIndex = -1;
+                for(int j = i+1; j < len; j++)  // to find endIndex of ']' close brace
+                {
+                    if(input.charAt(j) == '[') 
+                    {
+                        braceCount++;
+                    }
+                    else if(input.charAt(j) == ']') 
+                    {
+                        braceCount--;
+                        if(braceCount == 0) 
+                        {
+                            endIndex = j;
+                            break;
+                        }
+                    }
+                }  
+                
+                String subOutput = decodeString(input.substring(i+2, endIndex)); // recursion of substring
+                for(int j = 0; j < count; j++) 
+                {
+                	 output = output.append(subOutput);
+                }
+                i = endIndex; // substring got processed 
+            }
+            else if(Character.isLetter(ch))
+            {
+                output = output.append(ch);
+            }
+        }
+        return output.toString();
+    }
+}
---
+++ algo/0448. Find All Numbers Disappeared in an Array.java
@@ -0,0 +1,40 @@
+/*
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+Find all the elements of [1, n] inclusive that do not appear in this array.
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+Input: [4,3,2,7,8,2,3,1]
+Output: [5,6]
+*/
+
+class Solution 
+{
+    /*
+        1) swap algo (similar to find first missing positive number - problem 41)
+        2) add n and then % n using "nums[nums[i]]"
+        3) make as negative using "nums[nums[i]]"
+    */
+    
+    public List<Integer> findDisappearedNumbers(int[] nums) 
+    {
+        List<Integer> output = new ArrayList<Integer>();
+        int index;
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            index = Math.abs(nums[i]) - 1;
+            if(nums[index] > 0) { 
+                nums[index] *= -1;
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] > 0) {
+                output.add(i+1);
+            }
+        }
+        return output;
+    }
+}
