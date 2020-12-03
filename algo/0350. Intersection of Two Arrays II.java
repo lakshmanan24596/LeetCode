8ae9da2944e0ebb8d37ff63a9057052e0e5aab65
lakshmanan24596 <lakshmanan24596@gmail.com>
2020-12-03 07:09:24 +0530

    update

--- algo/0350. Intersection of Two Arrays II.java
+++ algo/0350. Intersection of Two Arrays II.java
@@ -1,4 +1,25 @@
 /*
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+
+Note:
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+
+Follow up:
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+*/
+
+/*
     follow up ques:
         1) if input is sorted, then no need extra space and we can use 2 pointer technique
         2) if nums1 size is smaller, then use hashmap for smaller size array
