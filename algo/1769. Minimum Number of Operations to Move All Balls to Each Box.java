c25420caa3f0bb57a1dab66abf0b93c6f01f6932
lakshmanan24596 <lakshmanan24596@gmail.com>
2021-04-09 01:07:12 +0530

    add

---
+++ algo/1769. Minimum Number of Operations to Move All Balls to Each Box.java
@@ -0,0 +1,116 @@
+/*
+You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+In one operation, you can move one ball from a box to an adjacent box. 
+Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
+Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
+Each answer[i] is calculated considering the initial state of the boxes.
+
+Example 1:
+Input: boxes = "110"
+Output: [1,1,3]
+Explanation: The answer for each box is as follows:
+1) First box: you will have to move one ball from the second box to the first box in one operation.
+2) Second box: you will have to move one ball from the first box to the second box in one operation.
+3) Third box: you will have to move one ball from the first box to the third box in two operations, and move one ball from the second box to the third box in one operation.
+
+Example 2:
+Input: boxes = "001011"
+Output: [11,8,5,4,3,4]
+
+Constraints:
+n == boxes.length
+1 <= n <= 2000
+boxes[i] is either '0' or '1'.
+*/
+
+
+
+/*
+    Logic: Math
+        consider example 2 --> index of 1 are 2, 4, 5
+        2 + 4 + 5
+        1 + 3 + 4
+        0 + 2 + 3
+        1 + 1 + 2
+        2 + 0 + 1
+        3 + 1 + 0
+        we can see that the values keep on decreases and then increases
+        use math to calculate this
+
+    Implementation:
+        operations = 2 + 4 + 5 = 11
+        noOfBalls = 3
+        0) 11 + 0
+        1) 8 + 0
+        2) 5 + 0
+        3) 2 + 2
+        4) -1 + 4
+        5) -4 + 8
+        
+    Complexity:
+        time: 2n
+        space: n for output
+*/
+/*
+class Solution {
+    public int[] minOperations(String boxes) {
+        int noOfBoxes = boxes.length();
+        int noOfBalls = 0;
+        int[] minOperations = new int[noOfBoxes];
+        int operations = 0;
+        int addition = 0;
+        int processedBalls = 0;
+        
+        for (int i = 0; i < noOfBoxes; i++) {
+            if (boxes.charAt(i) == '1') {
+                operations += i;
+                noOfBalls++;
+            }
+        }
+        
+        for (int i = 0; i < noOfBoxes; i++) {
+            minOperations[i] = operations + addition;       // main logic
+            
+            if (boxes.charAt(i) == '1') {
+                processedBalls++;
+            }
+            addition += 2 * processedBalls;
+            operations -= noOfBalls;
+        }
+        return minOperations;
+    }
+}
+*/
+
+
+/*
+    logic: LTR + RTL, greedy
+    time: 2n
+    space: n for output
+    
+    example-2:
+    left  = 0, 0, 0, 1, 2, 4
+    right = 11, 8, 5, 3, 1, 0
+    ans = left + right
+*/
+class Solution {
+    public int[] minOperations(String boxes) {
+        int noOfBoxes = boxes.length();
+        int[] minOperations = new int[noOfBoxes];
+        int left = 0, right = 0, count = 0;
+        
+        for (int i = 0; i < noOfBoxes; i++) {
+            minOperations[i] = left;
+            count += boxes.charAt(i) - '0';
+            left += count;
+        }
+        
+        count = 0;
+        for (int i = noOfBoxes - 1; i >= 0; i--) {
+            minOperations[i] += right;
+            count += boxes.charAt(i) - '0';
+            right += count;
+        }
+        return minOperations;
+    }
+}
